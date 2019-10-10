2c7ae132611b98460efb3c4ca89a02f7990acaca
yoshikawa-kohei <yoshikawa@ai.lab.uec.ac.jp>
2019-10-10 19:51:53 +0900

    modify execute file

--- execute-sim-rsfar.R
+++ execute-sim-rsfar.R
@@ -2,12 +2,12 @@ library(secure)
 source("R/rvsmanopt.R")
 source("R/generate-dataset.R")
 
-# generate dataset ------------------------------------------------------------
+# Generate dataset ------------------------------------------------------------
 n <- 400; p <- 120; q <- 60; nrank <- 12
 s2n <- 0.5; rho <- 0.3
 dataset <- generate.data(n, p , q, nrank, s2n, rho)
 
-# load data from dataset --------------------------------------------------
+# Perform RVSManOpt -----------------------------------------------------------
 rx <- qr(dataset$X)$rank
 max.rank.est <- min(rx, dataset$q)
 
